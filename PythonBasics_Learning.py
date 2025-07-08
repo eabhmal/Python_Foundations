23149c2f820e86cee13bec7db06008cfb781c925
Abhishek Malik <malik.abhishek88@gmail.com>
2025-07-07 21:18:35 -0400

    Second day practice

--- PythonBasics_Learning.py
+++ PythonBasics_Learning.py
@@ -17,3 +17,49 @@ for number in range(1, 4):
 
 for number in range(1, 4, 2):
     print("Attempt", number)
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+
+count = 0
+for x in range(1, 10):
+    if x % 2 == 0:
+        count = count + 1
+        print(x)
+print(f"We have {count} even numbers")
+
+
+def greet(first_name, last_name):
+    print(f"Hi there {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greet("Abhishek", "Malik")
+
+
+def increment(number, by):
+    return number + by
+
+
+print(increment(2, 1))
+message = increment(2, 1)
+print(message)
+
+
+def multiply(x, y):
+    return x*y
+
+
+multiply(2, 3)
+
+
+def multiply_n(*numbers):
+    total = 1
+    for number in numbers:
+        total = total * number
+    return total
+
+
+print(multiply_n(2, 3, 4, 5))
